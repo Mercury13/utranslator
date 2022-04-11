005583ba84ef92d8f521d683aff6ba3182b8beed
Mercury13 <mercury13@ukr.net>
2022-04-12 00:28:14 +0300

    Fixed setGeometry

--- UTranslator/main.cpp
+++ UTranslator/main.cpp
@@ -1,9 +1,37 @@
 #include "FmMain.h"
 
 #include <QApplication>
+#include <QGuiApplication>
+#include <QScreen>
 
 // Project-local
 #include "d_Config.h"
+
+template <class T>
+inline T clampMin(const T& val, const T& min, const T& max)
+    { return std::max(std::min(val, max), min); }
+
+void setGeometry(QMainWindow& win, const QRect& rect)
+{
+    static constexpr int BUFFER_ZONE = 64;  // We should see ? px of window
+    static constexpr int HEADER_ZONE = 24;  // We should see ? px of header
+
+    QRect desktopRect = QApplication::screens()[0]->availableVirtualGeometry();
+
+    // Reduce width-height
+    auto w = std::min(rect.width(), desktopRect.width());
+    auto h = std::min(rect.height(), desktopRect.height());
+
+    // Move x-y
+    auto x = clampMin(rect.left(),
+                      desktopRect.left() + BUFFER_ZONE - w,
+                      desktopRect.right() - BUFFER_ZONE);
+    auto y = clampMin(rect.top(),
+                      desktopRect.top() + BUFFER_ZONE,
+                      desktopRect.bottom() - HEADER_ZONE);
+
+    win.setGeometry(x, y, w, h);
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,7 +42,7 @@ int main(int argc, char *argv[])
     {
         auto rect = w.geometry();
         config::init(rect);
-        w.setGeometry(rect);
+        setGeometry(w, rect);
         if (config::window::isMaximized)
             w.setWindowState(w.windowState() | Qt::WindowMaximized);
     }
